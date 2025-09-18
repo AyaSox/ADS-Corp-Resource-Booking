a1f75bf61ebdf88fc4e5331de46c8759a54060a2
Your Full Name <your.email@example.com>
2025-09-18 20:09:44 +0200

    chore: exclude sqlite journal files from commit

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-# Render Build Script
-#!/bin/bash
-
-# Install .NET SDK and restore packages
-dotnet restore
-dotnet publish -c Release -o ./publish --self-contained false --runtime linux-x64
--- render-start.sh
+++
@@ -1,4 +0,0 @@
-# Render Start Script
-#!/bin/bash
-cd publish
-dotnet ResourceBooking.dll --urls "http://0.0.0.0:$PORT"
